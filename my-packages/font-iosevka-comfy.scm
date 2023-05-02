43aaa646c3356ffeba2f35494472dd5285be3fca
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2023-05-02 17:24:35 +0200

    Added font-iosevka-comfy.scm
    Signed-off-by: Arnaud Lechevallier <arnaud.lechevallier@free.fr>

---
+++ my-packages/font-iosevka-comfy.scm
@@ -0,0 +1,24 @@
+(define-module (my-packages font-iosevka-comfy)
+  #:use-module (guix packages)
+  #:use-module (gnu packages fonts)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system font)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define-public font-iosevka-comfy
+  (package
+   (name "font-iosevka-comfy")
+   (version "1.1.1")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+		  (url "https://github.com/protesilaos/iosevka-comfy.git")
+		  (commit (string-append "" version))))
+	    (file-name (git-file-name name version))
+	    (sha256
+	     (base32 "0qzqz0b4k0bm93l6k8bfbqmy04cfy8vbbv4xwf9iyrb305siwjjy"))))
+   (build-system font-build-system)
+   (home-page "https://github.com/protesilaos/iosevka-comfy")
+   (synopsis "A TTF font derived from Iosevka")
+   (description "Iosevka Comfy is a TTF font derived from Iosevka. It is a slightly tweaked version of the original, designed for enhanced readability.")
+   (license license:silofl1.1)))
