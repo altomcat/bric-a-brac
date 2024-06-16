aa778dc83d5f77e4b3006141506873db899d0bc4
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2024-06-16 09:47:55 +0200

    Added a simple package definition to build `guile-pstk'.

---
+++ my-packages/guile-pstk.scm
@@ -0,0 +1,37 @@
+(define-module (my-packages guile-ptsk)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system guile)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages tcl)
+  )
+
+(define-public guile-ptsk
+  (package
+   (name "guile-ptsk")
+   (version "0.0.0")
+   (source (origin
+	    (method git-fetch)
+	    (uri (git-reference
+		  (url "https://github.com/KikyTokamuro/guile-pstk")
+		  ;;(url "https://codeberg.org/kakafarm/guile-pstk.git")
+		  (commit "70396a6c49be6829b8fb6c3815bcb06dd17b2874")))
+	    (file-name (git-file-name name version))
+	    (sha256
+	     (base32 "0az4dxfw33q01p6rrvs7l847m7qf1626299mk4gfavz37km5l99h"))))
+   (build-system guile-build-system)
+   ;; (arguments '())
+   (inputs
+    (list guile-3.0
+	  tk
+	  tcl))
+   (home-page "https://github.com/KikyTokamuro/guile-pstk")
+   ;;(home-page "https://codeberg.org/kakafarm/guile-pstk")
+   (synopsis "Guile implementation of PS/Tk, a portable Scheme interface to Tk GUI toolkit.")
+   (description
+    "PS/TK version (http://mirror.informatimago.com/scheme/www.t3x.org/pstk/index.html) fixed to work fine on modern GNU Guile")
+   (license license:bsd-3)))
+
+;;guile-ptsk
