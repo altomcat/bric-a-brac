36ea84609062f7e05c4dc8db7831f8a9869edbb6
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2024-08-17 11:51:04 +0200

    Renamed `raylib-guile' to `guile-raylib'.

--- bric-a-brac/packages/game-development.scm
+++ bric-a-brac/packages/game-development.scm
@@ -24,9 +24,14 @@
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;; My attempt to create a guix definition package for `raylib-guile'
-;;; On segmentation fault due to a conflict from several GLFW libraries installed on your system,
-;;; try to run it with `guix shell guile raylib-guile --pure'
+;;; My attempt to create a guix definition package for `guile-raylib'.
+;;;
+;;; If you encounter the following error:
+;;; WARNING: GLFW: Error: 65544 Description: Wayland: Failed to load libwayland-client
+;;; WARNING: GLFW: Failed to initialize GLFW
+;;;
+;;; Try to run it with: `unset WAYLAND-DISPLAY; guix shell guile guile-raylib`
+;;; or `guix shell guile guile-raylib --pure'
 
 
 (define-module (bric-a-brac packages game-development)
@@ -45,7 +50,7 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages gl)
   #:export (raylib-with-extras)
-  #:export (raylib-guile))
+  #:export (guile-raylib))
 
 
 (define-public raylib-with-extras
@@ -94,93 +99,93 @@
     (propagated-inputs (list glfw-3.4))
     ))
 
-(define-public raylib-guile
+(define-public guile-raylib
   (package
-    (name "raylib-guile")
-    (version "0.0.0")
-    (source (origin
-	      (method git-fetch)
-	      (uri (git-reference
-		    (url "https://github.com/petelliott/raylib-guile.git")
-                    (commit "e6b2ac8a21ac83c426ece993daa90ac04a90d0c2")))
-	      (file-name (git-file-name name version))
-	      (sha256
-	       (base32 "114v2rcwqyczqw80hzm6ij8iqfr93x43kj8qkq8gk7w49wcq8c5c"))))
-    (build-system gnu-build-system)
-    (native-inputs (list pkg-config))
-    (inputs (list guile-3.0
-		  guile-lib))
-    (propagated-inputs (list raylib-with-extras))
-    (arguments
-     `( #:make-flags '("GUILE_AUTO_COMPILE=0")
-       #:tests? #f
-       #:modules
-       ((guix build gnu-build-system)
-	(guix build utils)
-	(srfi srfi-1))
-       #:imported-modules ((guix build guile-build-system)
-			   ,@%gnu-build-system-modules)
-       #:phases
-       (modify-phases %standard-phases
-	 (delete 'configure)
-	 (delete 'strip)
+   (name "guile-raylib")
+   (version "0.0.0")
+   (source (origin
+	    (method git-fetch)
+	    (uri (git-reference
+		  (url "https://github.com/petelliott/raylib-guile.git")
+                  (commit "e6b2ac8a21ac83c426ece993daa90ac04a90d0c2")))
+	    (file-name (git-file-name name version))
+	    (sha256
+	     (base32 "114v2rcwqyczqw80hzm6ij8iqfr93x43kj8qkq8gk7w49wcq8c5c"))))
+   (build-system gnu-build-system)
+   (native-inputs (list pkg-config))
+   (inputs (list guile-3.0
+		 guile-lib))
+   (propagated-inputs (list raylib-with-extras))
+   (arguments
+    `( #:make-flags '("GUILE_AUTO_COMPILE=0")
+      #:tests? #f
+      #:modules
+      ((guix build gnu-build-system)
+       (guix build utils)
+       (srfi srfi-1))
+      #:imported-modules ((guix build guile-build-system)
+			  ,@%gnu-build-system-modules)
+      #:phases
+      (modify-phases %standard-phases
+		     (delete 'configure)
+		     (delete 'strip)
 
-	 (add-before 'build 'my-build
-	   (lambda* (#:key inputs #:allow-other-keys)
-	     (let ((raylib-version (last (string-split (assoc-ref inputs "raylib") #\-)))
-		   (raylib-xml (string-append
-				(assoc-ref inputs "raylib")
-				"/parser/output/raylib_api.xml")))
-	       (copy-file raylib-xml (string-append (getcwd) "/raylib_api.xml"))
-	       #t)))
+		     (add-before 'build 'my-build
+				 (lambda* (#:key inputs #:allow-other-keys)
+				   (let ((raylib-version (last (string-split (assoc-ref inputs "raylib") #\-)))
+					 (raylib-xml (string-append
+						      (assoc-ref inputs "raylib")
+						      "/parser/output/raylib_api.xml")))
+				     (copy-file raylib-xml (string-append (getcwd) "/raylib_api.xml"))
+				     #t)))
 
-	 (add-after 'unpack 'fix-makefile
-	   (lambda _
-	     (use-modules (guix build guile-build-system))
-	     (substitute* "Makefile"
-	       (("--cflags guile-3.0") (format #f "--cflags guile-~a raylib"
-					       (target-guile-effective-version))))
-	     (substitute* "Makefile"
-	       (("-lraylib") "`pkg-config --libs raylib`"))
-	     (substitute* "Makefile"
-	       (("install: all") "do-not-install:"))
-	     ))
+		     (add-after 'unpack 'fix-makefile
+				(lambda _
+				  (use-modules (guix build guile-build-system))
+				  (substitute* "Makefile"
+					       (("--cflags guile-3.0") (format #f "--cflags guile-~a raylib"
+									       (target-guile-effective-version))))
+				  (substitute* "Makefile"
+					       (("-lraylib") "`pkg-config --libs raylib`"))
+				  (substitute* "Makefile"
+					       (("install: all") "do-not-install:"))
+				  ))
 
-	 (add-after 'fix-makefile 'fix-guile-extensions
-	   (lambda* (#:key inputs outputs #:allow-other-keys)
-	     (use-modules (guix build guile-build-system))
-	     (substitute* (find-files "." ".*\\.scm")
-	       (("libraylib-guile") (format #f "~a/lib/guile/~a/extensions/libraylib-guile"
-					    (assoc-ref outputs "out")
-					    (target-guile-effective-version))))
-	     #t))
+		     (add-after 'fix-makefile 'fix-guile-extensions
+				(lambda* (#:key inputs outputs #:allow-other-keys)
+				  (use-modules (guix build guile-build-system))
+				  (substitute* (find-files "." ".*\\.scm")
+					       (("libraylib-guile") (format #f "~a/lib/guile/~a/extensions/libraylib-guile"
+									    (assoc-ref outputs "out")
+									    (target-guile-effective-version))))
+				  #t))
 
-	 (add-after 'build 'compile-and-install-guile-object
-	   (lambda* (#:key inputs outputs #:allow-other-keys)
-	     (use-modules (guix build guile-build-system))
-	     (let* ((out (assoc-ref outputs "out"))
-		    (scm "raylib.scm")
-		    (go "raylib.go")
-		    (lib "libraylib-guile.so")
-		    (effective-version (target-guile-effective-version))
-		    (lib-out (string-append out "/lib/guile/" effective-version))
-		    (share-out (string-append out "/share/guile/site/" effective-version)))
-	       ;; get rid off warnings
-	       (setenv "GUILE_AUTO_COMPILE" "0")
-	       (invoke "guild" "compile" scm "-o" go)
-	       (install-file scm (string-append share-out ))
-	       (install-file go (string-append lib-out "/site-ccache"))
-	       (install-file lib (string-append lib-out "/extensions"))
-	       #t)))
-	 )))
+		     (add-after 'build 'compile-and-install-guile-object
+				(lambda* (#:key inputs outputs #:allow-other-keys)
+				  (use-modules (guix build guile-build-system))
+				  (let* ((out (assoc-ref outputs "out"))
+					 (scm "raylib.scm")
+					 (go "raylib.go")
+					 (lib "libraylib-guile.so")
+					 (effective-version (target-guile-effective-version))
+					 (lib-out (string-append out "/lib/guile/" effective-version))
+					 (share-out (string-append out "/share/guile/site/" effective-version)))
+				    ;; get rid off warnings
+				    (setenv "GUILE_AUTO_COMPILE" "0")
+				    (invoke "guild" "compile" scm "-o" go)
+				    (install-file scm (string-append share-out ))
+				    (install-file go (string-append lib-out "/site-ccache"))
+				    (install-file lib (string-append lib-out "/extensions"))
+				    #t)))
+		     )))
 
 
-    (home-page "https://github.com/petelliott/raylib-guile.git")
-    (synopsis "Guile bindings for raylib library.")
-    (description
-     "This package provides GNU Guile bindings to the Raylib game development library.")
-    (license license:zlib)))
+   (home-page "https://github.com/petelliott/raylib-guile.git")
+   (synopsis "Guile bindings for raylib library.")
+   (description
+    "This package provides GNU Guile bindings to the Raylib game development library.")
+   (license license:zlib)))
 
-;; Uncommnent to install with `guix package -f raylib-guile.scm'
+;; Uncommnent to install with `guix package -f guile-raylib.scm'
 ;; raylib-with-extras
-;; raylib-guile
+guile-raylib
