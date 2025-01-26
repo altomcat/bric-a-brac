eba25442465289c11e23b2daf1256211b393444a
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2025-01-26 22:18:47 +0100

    Cleaned up a bit.

--- bric-a-brac/packages/emacs-xyz.scm
+++ bric-a-brac/packages/emacs-xyz.scm
@@ -5,7 +5,7 @@
 ;; Author: Arnaud Lechevallier <arnaud.lechevallier@free.fr>
 ;; Maintener: Arnaud Lechevallier <arnaud.lechevallier@free.fr>
 ;; Created: 2024/08/11
-;; Version: 0.0.1
+;; Version: 0.0.2
 
 ;; This file is part of GNU Emacs.
 
@@ -35,39 +35,57 @@
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages emacs-xyz)
   ;; #:export (emacs-substitute)
-  ;; #:export (emacs-org-appear-0.3.1)
   #:export (emacs-consult-denote)
   #:export (emacs-hasliberg-theme)
   #:export (emacs-miasma-theme)
-  #:export (emacs-odin-mode)
-  )
+  #:export (emacs-odin-mode))
+
+(define emacs-odin-mode
+  (package
+   (name "emacs-odin-mode")
+   (version "0.0.0")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/mattt-b/odin-mode.git")
+           (commit version)))
+     (file-name (git-file-name name version))
+     (sha256
+      (base32 "1hfhnzhmbxrw4kz977s48x4nbq86vda5dvj00s2ima2i22b8l2z4"))))
+   (build-system emacs-build-system)
+   (home-page "https://github.com/mattt-b/odin-mode.git")
+   (synopsis "Emacs major mode for editing Odin code")
+   (description "This package provides an Emacs major mode for highlighting
+code written in the Odin programming language.")
+   (license license:expat)))
 
 (define emacs-hasliberg-theme
   (package
-    (name "emacs-hasliberg-theme")
-    (version "0.0")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/rytswd/hasliberg-theme.git")
-                    (commit "2188dcc77aec78581164ced8608b5ba23eca8859")))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "19b7hi771v5qfbv3y4gqzhhsgc3bdzgh3jdvnimxyvhx3l3cgrwh"))))
-    (build-system emacs-build-system)
-    (arguments
-     (list
-      #:phases
-      #~(modify-phases %standard-phases
-          (add-after 'unpack 'require-cl-lib
-            (lambda _
-              (substitute* "hasliberg-theme.el"
-                ((";;; Code:") ";;; Code:\n(require 'cl-lib)")))))))
-    (home-page "https://github.com/rytswd/hasliberg-theme")
-    (synopsis "An Emacs dark theme inspired by Swiss Alps.")
-    (description "Hasliberg theme for Emacs has been designed for readability based on the LCH color space, to achieve a more homogenous color gradient for the theme.")
-    (license license:gpl3)))
+   (name "emacs-hasliberg-theme")
+   (version "0.0")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/rytswd/hasliberg-theme.git")
+                  (commit "2188dcc77aec78581164ced8608b5ba23eca8859")))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "19b7hi771v5qfbv3y4gqzhhsgc3bdzgh3jdvnimxyvhx3l3cgrwh"))))
+   (build-system emacs-build-system)
+   (arguments
+    (list
+     #:phases
+     #~(modify-phases %standard-phases
+                      (add-after 'unpack 'require-cl-lib
+                                 (lambda _
+                                   (substitute* "hasliberg-theme.el"
+                                                ((";;; Code:") ";;; Code:\n(require 'cl-lib)")))))))
+   (home-page "https://github.com/rytswd/hasliberg-theme")
+   (synopsis "An Emacs dark theme inspired by Swiss Alps.")
+   (description "Hasliberg theme for Emacs has been designed for readability based on the LCH color space, to achieve a more homogenous color gradient for the theme.")
+   (license license:gpl3)))
 
 (define emacs-miasma-theme
   (let ((commit "76517179825b1af35db1dae7c73f615eacd2c973")
@@ -93,22 +111,22 @@
 
 (define emacs-substitute
   (package
-    (name "emacs-substitute")
-    (version "0.1.8")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://git.sr.ht/~protesilaos/substitute")
-                    (commit "b81bb7789847f3d1645a60422fa080c48b93dd47")))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "0s1lgpwmdjz17rd4r6qp41agmpqwa9976v1pb55p648zjh0p2vlz"))))
-    (build-system emacs-build-system)
-    (home-page "https://git.sr.ht/~protesilaos/substitute")
-    (synopsis "Efficiently replace targets in the buffer or context")
-    (description
-     "Substitute is a set of commands that perform text replacement (i) throughout
+   (name "emacs-substitute")
+   (version "0.1.8")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://git.sr.ht/~protesilaos/substitute")
+                  (commit "b81bb7789847f3d1645a60422fa080c48b93dd47")))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "0s1lgpwmdjz17rd4r6qp41agmpqwa9976v1pb55p648zjh0p2vlz"))))
+   (build-system emacs-build-system)
+   (home-page "https://git.sr.ht/~protesilaos/substitute")
+   (synopsis "Efficiently replace targets in the buffer or context")
+   (description
+    "Substitute is a set of commands that perform text replacement (i) throughout
       the buffer, (ii) limited to the current definition (per narrow-to-defun),
       (iii) from point to the end of the buffer, and (iv) from point to the beginning
        of the buffer.
@@ -116,69 +134,32 @@
      These substitutions are meant to be as quick as possible and, as such, differ
       from the standard query-replace (which I still use).  The provided commands
        prompt for substitute text and perform the substitution outright.")
-    (license license:gpl1+)))
-
-(define emacs-org-appear-0.3.1
-  (package
-    (inherit emacs-org-appear)
-    (name "emacs-org-appear")
-    (version "0.3.1")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/awth13/org-appear")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "1hfhnzhmbxrw4kz977s48x4nbq86vda5dvj00s2ima2i22b8l2z4"))))
-    ))
+   (license license:gpl1+)))
 
 (define emacs-consult-denote
   (package
-    (name "emacs-consult-denote")
-    (version "0.2.2")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/protesilaos/consult-denote")
-		    (commit "9fbe0f6a2636f46928f02ddde9c454f36aa45f39")))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-	        "0f4xjmapxwx7k4r1m0q3czjcrb5cgn5qh8ar5yn1fbmyygykwc6z"))))
-    (build-system emacs-build-system)
-    (inputs
-     (list emacs-denote
-	   emacs-consult))
-    (home-page "https://github.com/protesilaos/consult-denote")
-    (synopsis "Consult integration for Denote")
-    (description
-     "This package integrates the @code{emacs-denote} package with Daniel Mendler's @code{emacs-consult}.
+   (name "emacs-consult-denote")
+   (version "0.2.2")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/protesilaos/consult-denote")
+		  (commit "9fbe0f6a2636f46928f02ddde9c454f36aa45f39")))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+	      "0f4xjmapxwx7k4r1m0q3czjcrb5cgn5qh8ar5yn1fbmyygykwc6z"))))
+   (build-system emacs-build-system)
+   (inputs
+    (list emacs-denote
+	  emacs-consult))
+   (home-page "https://github.com/protesilaos/consult-denote")
+   (synopsis "Consult integration for Denote")
+   (description
+    "This package integrates the @code{emacs-denote} package with Daniel Mendler's @code{emacs-consult}.
      The idea is to enhance minibuffer interactions, such as by providing a preview of the file-to-linked/opened
      and by adding more sources to the @code{consult-buffer} command.")
-
-    (license license:gpl3)))
-
-(define emacs-odin-mode
-  (package
-    (name "emacs-odin-mode")
-    (version "0.0.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/mattt-b/odin-mode.git")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "1hfhnzhmbxrw4kz977s48x4nbq86vda5dvj00s2ima2i22b8l2z4"))))
-    (build-system emacs-build-system)
-    (home-page "https://github.com/mattt-b/odin-mode.git")
-    (synopsis "Emacs major mode for editing Odin code")
-    (description "This package provides an Emacs major mode for highlighting
-code written in the Odin programming language.")
-    (license #f)))
+   (license license:gpl3)))
 
 ;; Uncommnent to install with `guix package -f emacs-substitute.scm'
 ;; emacs-substitute
