b5ce0bf7c623a453777dc2c3136118217a1f7310
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2025-02-01 13:57:16 +0100

    Fixed a missing library leading to crash when running the demo.

--- bric-a-brac/packages/odin.scm
+++ bric-a-brac/packages/odin.scm
@@ -5,7 +5,7 @@
 ;; Author: Arnaud Lechevallier <arnaud.lechevallier@free.fr>
 ;; Maintener: Arnaud Lechevallier <arnaud.lechevallier@free.fr>
 ;; Created: 2025/01/16
-;; Version: 0.0.1
+;; Version: 0.0.2
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -43,84 +43,93 @@
   (let ((commit "2aae4cfd461860bd10dcb922f867c98212a11449")
         (revision "dev-2025-01"))
     (package
-     (name "odin")
-     (version (git-version "0.0" revision commit))
-     (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/odin-lang/Odin.git")
-                    (commit commit)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "17xgyr0xsg3bdfn472kniyld813vprm8g0x99qhj05w8wgirlxqr"))))
-     ;; Useful to apply changes locally and confirm the right behavior
-     ;; of the package we are building afterward
-     ;; (source (local-file "../../../projects/odin/Odin" "odin-checkout"
-     ;;                     #:recursive? #t))
-     (build-system gnu-build-system)
-     (arguments
-      (list #:tests? #f
-            #:strip-binaries? #f
-            #:phases
-            #~(modify-phases %standard-phases
-                             (delete 'configure)
-                             (delete 'validate-runpath)
-                             (delete 'strip)
-                             (delete 'make-dynamic-linker-cache)
-                             (replace 'build
-                                      (lambda _
-                                        (invoke "./build_odin.sh"
-                                                "release-native")
-                                        #t))
-                             (replace 'install
-                                      (lambda* (#:key inputs outputs #:allow-other-keys)
-                                        (let* ((odin "odin")
-                                               (src (assoc-ref inputs "source"))
-                                               (src-base (string-append src "/base"))
-                                               (src-core (string-append src "/core"))
-                                               (src-vendor (string-append src "/vendor"))
-                                               (out (assoc-ref outputs "out"))
-                                               (out-base (string-append out "/base"))
-                                               (out-core (string-append out "/core"))
-                                               (out-vendor (string-append out "/vendor"))
-                                               (bin (string-append out "/bin")))
-                                          (install-file odin bin)
-                                          (copy-recursively src-base out-base)
-                                          (copy-recursively src-core out-core)
-                                          (copy-recursively src-vendor out-vendor))
-                                        #t))
-                             (add-after 'install 'wrap-odin
-                                        (lambda* (#:key inputs outputs #:allow-other-keys)
-                                          (let* ((out (assoc-ref outputs "out"))
-                                                 (bin (string-append out "/bin/odin"))
-                                                 (llvm-lib (string-append (assoc-ref inputs "llvm")
-                                                                          "/lib"))
-                                                 (clang-lib (string-append (assoc-ref inputs "clang-toolchain")
-                                                                           "/lib")))
-                                            (wrap-program bin
-                                                          `("ODIN_ROOT" = (,out))
-                                                          `("LD_LIBRARY_PATH" = ( ,llvm-lib
-                                                                                  ,clang-lib))))
-                                          #t))
-                             )))
-     (native-inputs
-      (list llvm-19
-            clang-toolchain-19
-            python-3
-            which))
-     (inputs
-      (list clang-toolchain-19))
-     (home-page "https://github.com/nakst/gf")
-     (synopsis "A modern, fast, and simple systems programming language")
-     (description
-      "The Odin programming language is a modern systems language focused on simplicity,
+      (name "odin")
+      (version (git-version "0.0" revision commit))
+      ;; works preferrably on a local directory, otherwise from the git repository
+      (source
+       (let ((local-source  "../../../projects/odin/Odin"))
+         (if (file-exists? local-source)
+             (local-file local-source "odin-checkout"
+                         #:recursive? #t)
+             (origin
+               (method git-fetch)
+               (uri (git-reference
+                     (url "https://github.com/odin-lang/Odin.git")
+                     (commit commit)))
+               (file-name (git-file-name name version))
+               (sha256
+                (base32
+                 "17xgyr0xsg3bdfn472kniyld813vprm8g0x99qhj05w8wgirlxqr"))))))
+      (build-system gnu-build-system)
+      (arguments
+       (list #:tests? #f
+             #:strip-binaries? #f
+             #:phases
+             #~(modify-phases %standard-phases
+                 (delete 'configure)
+                 (replace 'build
+                   (lambda _
+                     ;; FIXME: how to avoid the usage of patchelf
+                     (substitute* "build_odin.sh"
+                       (("\\./odin run examples.*" all)
+                        (format #f "~a ~a    ~a~%"
+                                "LD_PRELOAD=libgcc_s.so.1"
+                                all
+                                "patchelf --add-needed libgcc_s.so.1 ./demo")))
+                     (invoke "./build_odin.sh")
+                     #t))
+                 (replace 'install
+                   (lambda* (#:key inputs outputs #:allow-other-keys)
+                     (let* ((odin "odin")
+                            (demo "demo")
+                            (demo-odin (string-append  demo "-" odin))
+                            (src (assoc-ref inputs "source"))
+                            (src-base (string-append src "/base"))
+                            (src-core (string-append src "/core"))
+                            (src-vendor (string-append src "/vendor"))
+                            (out (assoc-ref outputs "out"))
+                            (out-base (string-append out "/base"))
+                            (out-core (string-append out "/core"))
+                            (out-vendor (string-append out "/vendor"))
+                            (bin (string-append out "/bin")))
+                       (install-file odin bin)
+                       (rename-file demo demo-odin)
+                       (install-file demo-odin bin)
+                       (copy-recursively src-base out-base)
+                       (copy-recursively src-core out-core)
+                       (copy-recursively src-vendor out-vendor))
+                     #t))
+                 (add-after 'install 'wrap-odin
+                   (lambda* (#:key inputs outputs #:allow-other-keys)
+                     (let* ((out (assoc-ref outputs "out"))
+                            (bin (string-append out "/bin/odin"))
+                            (llvm-lib (string-append (assoc-ref inputs "llvm")
+                                                     "/lib"))
+                            (clang-lib (string-append (assoc-ref inputs "clang-toolchain")
+                                                      "/lib")))
+                       (wrap-program bin
+                         `("ODIN_ROOT" = (,out))
+                         `("LD_LIBRARY_PATH" = (,llvm-lib
+                                                ,clang-lib))))
+                     #t)))))
+      (native-inputs
+       (list llvm-18
+             clang-toolchain-18
+             ;;python-3
+             which
+             patchelf))
+      (inputs
+       (list clang-toolchain-18))
+      (home-page "https://github.com/nakst/gf")
+      (synopsis "A modern, fast, and simple systems programming language")
+      (description
+       "The Odin programming language is a modern systems language focused on simplicity,
 performance, and productivity.  Designed as an alternative to C, it is suited for
 high-performance development, including game engines, graphics programming, and systems
 code.  Odin provides expressive syntax, support for data-oriented programming, a minimal
 runtime, strong compile-time efficiency, and seamless C interoperability.  This package
 includes the Odin compiler and standard library for building and running Odin programs.")
-     (license license:expat))))
+      (license license:expat))))
 
 ;; Uncomment to install with `guix package -f odin'
-;; odin
+odin
