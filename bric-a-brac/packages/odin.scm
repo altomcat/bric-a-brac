da284f00507c32b36f0ddbf8ee296858099dddf9
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2025-02-23 10:48:07 +0100

    Switched to dev-2025-02.
    Additional notes:
     * cleanup of embedded libraries directly from source definition
       (instead of using a phase).
     * built stb libraries from odin's vendor.

--- bric-a-brac/packages/odin.scm
+++ bric-a-brac/packages/odin.scm
@@ -42,116 +42,128 @@
   #:export (odin))
 
 (define odin
-  (let ((commit "2aae4cfd461860bd10dcb922f867c98212a11449")
-        (revision "dev-2025-01"))
+  (let ((commit "584fdc0d4ab9dcc01e218e499fdaef9969223d65")
+        (revision "dev-2025-02"))
     (package
-      (name "odin")
-      (version (git-version "0.0" revision commit))
-      ;; works preferrably on a local directory, otherwise from the git repository
-      (source
-       (let ((local-source  "../../../projects/odin/Odin"))
-         (if (file-exists? local-source)
-             (local-file local-source "odin-checkout"
-                         #:recursive? #t)
-             (origin
-               (method git-fetch)
-               (uri (git-reference
-                     (url "https://github.com/odin-lang/Odin.git")
-                     (commit commit)))
-               (file-name (git-file-name name version))
-               (sha256
-                (base32
-                 "17xgyr0xsg3bdfn472kniyld813vprm8g0x99qhj05w8wgirlxqr"))))))
-      (build-system gnu-build-system)
-      (arguments
-       (list #:tests? #f
-             #:strip-binaries? #f
-             #:phases
-             #~(modify-phases %standard-phases
-                 (delete 'configure)
-                 (replace 'build
-                   (lambda _
-                     ;; FIXME: how to avoid the usage of patchelf
-                     (substitute* "build_odin.sh"
-                       (("\\./odin run examples.*" all)
-                        (format #f "~a ~a    ~a~%"
-                                "LD_PRELOAD=libgcc_s.so.1"
-                                all
-                                "patchelf --add-needed libgcc_s.so.1 ./demo")))
-                     (invoke "./build_odin.sh")
-                     #t))
-                 (replace 'install
-                   (lambda* (#:key inputs outputs #:allow-other-keys)
-                     (let* ((odin "odin")
-                            (demo "demo")
-                            (demo-odin (string-append  demo "-" odin))
-                            (sources '("/base" "/core" "/vendor"))
-                            (bin (string-append #$output "/bin")))
-                       (install-file odin bin)
-                       (rename-file demo demo-odin)
-                       (install-file demo-odin bin)
-                       (for-each
-                        (lambda (folder)
-                          (copy-recursively (string-append #$source folder)
-                                            (string-append #$output folder)))
-                        sources))
-                     #t))
-                 (add-after 'install 'remove-static-libraries
-                   (lambda* (#:key outputs #:allow-other-keys)
-                     (let ((out (string-append #$output
-                                               "/vendor")))
-                       (for-each delete-file
-                                 (find-files out "\\.(a|lib|dll|so(\\.[0-9]+)*)$")))
-                     #t))
-                 (add-after 'remove-static-libraries 'replace-static-libraries
-                   (lambda* (#:key inputs outputs #:allow-other-keys)
-                     (let* ((target-system #$(or (%current-target-system)
-                                                 (%current-system)))
-                            (box2d-lib-out (string-append #$output "/vendor/box2d/lib/"))
-                            (raylib-lib-out (string-append #$output "/vendor/raylib/linux/"))
-                            (glfw-lib-out (string-append #$output "/vendor/glfw/lib/"))
-                            (box2d-lib (string-append #$box2d+static "/lib/libbox2d.a"))
-                            (raylib-lib (string-append #$raylib-with-extras+static "/lib/libraylib.a"))
-                            (glfw-lib (string-append #$glfw+static "/lib/libglfw3.a")))
-                       (cond
-                        ((string-prefix? "x86_64-linux" target-system)
-                         (copy-file box2d-lib (string-append box2d-lib-out "box2d_other_amd64_avx2.a"))
-                         (install-file raylib-lib raylib-lib-out)
-                         (install-file glfw-lib glfw-lib-out)
-                         )
-                        (else
-                         '())))))
-                 (add-after 'install 'wrap-odin
-                   (lambda* (#:key inputs outputs #:allow-other-keys)
-                     (let* ((bin (string-append #$output "/bin/odin"))
-                            (llvm-lib (string-append #$llvm "/lib"))
-                            (clang-lib (string-append #$clang-toolchain "/lib")))
-                       (wrap-program bin
-                         `("ODIN_ROOT" = (,#$output))
-                         `("LD_LIBRARY_PATH" = (,llvm-lib
-                                                ,clang-lib))))
-                     #t)))))
-      (native-inputs
-       (list llvm-18
-             clang-toolchain-18
-             ;;python-3
-             which
-             patchelf
-             box2d+static
-             glfw+static
-             raylib-with-extras+static))
-      (inputs
-       (list clang-toolchain-18))
-      (home-page "https://github.com/nakst/gf")
-      (synopsis "A modern, fast, and simple systems programming language")
-      (description
-       "The Odin programming language is a modern systems language focused on simplicity,
+     (name "odin")
+     (version (git-version "0.0" revision commit))
+     ;; works preferrably on a local directory, otherwise from the git repository
+     (source
+      (let ((local-source  "../../../projects/odin/Odin"))
+        (if (file-exists? local-source)
+            (local-file local-source "odin-checkout"
+                        #:recursive? #t)
+            (origin
+             (method git-fetch)
+             (uri (git-reference
+                   (url "https://github.com/odin-lang/Odin.git")
+                   (commit commit)))
+             (file-name (git-file-name name version))
+             (sha256
+              (base32
+               "12y4rjssfryyyjxkkjnx32363xf6nmd6hnxgbl235z487jla9ag0"))
+             (modules '((guix build utils)))
+             (snippet
+              '(begin
+                 ;; (for-each delete-file-recursively
+                 ;;           (find-files "vendor" "(darwin|macos|windows)" #:directories? #t))
+                 (for-each delete-file
+                           (find-files "vendor" "\\.(a|o|lib|dll|so(\\.[0-9]+)*)$"))))))))
+     (build-system gnu-build-system)
+     (arguments
+      (list #:tests? #f
+            #:strip-binaries? #f
+            #:phases
+            #~(modify-phases %standard-phases
+                             (delete 'configure)
+                             (replace 'build
+                                      (lambda _
+                                        ;; FIXME: how to avoid the usage of patchelf
+                                        (substitute* "build_odin.sh"
+                                                     (("\\./odin run examples.*" all)
+                                                      (format #f "~a ~a    ~a~%"
+                                                              "LD_PRELOAD=libgcc_s.so.1"
+                                                              all
+                                                              "patchelf --add-needed libgcc_s.so.1 ./demo")))
+                                        (invoke "./build_odin.sh")
+                                        #t))
+                             (replace 'install
+                                      (lambda* (#:key inputs outputs #:allow-other-keys)
+                                        (let* ((odin "odin")
+                                               (demo "demo")
+                                               (demo-odin (string-append  demo "-" odin))
+                                               (sources '("/base" "/core" "/vendor"))
+                                               (bin (string-append #$output "/bin")))
+                                          (install-file odin bin)
+                                          (rename-file demo demo-odin)
+                                          (install-file demo-odin bin)
+                                          (for-each
+                                           (lambda (folder)
+                                             (copy-recursively (string-append #$source folder)
+                                                               (string-append #$output folder)))
+                                           sources))
+                                        #t))
+                             (add-after 'install 'build-stb-static-libraries
+                                        (lambda* (#:key inputs outputs #:allow-other-keys)
+                                          (with-directory-excursion (string-append (getcwd) "/vendor/stb/src")
+                                                                    (setenv "CC" (which "gcc"))
+                                                                    (setenv "AR" (which "gcc-ar"))
+                                                                    (invoke (which "make") "unix"))
+                                          #t))
+                             (add-after 'build-stb-static-libraries 'replace-static-libraries
+                                        (lambda* (#:key inputs outputs #:allow-other-keys)
+                                          (let* ((target-system #$(or (%current-target-system)
+                                                                      (%current-system)))
+                                                 (box2d-lib-out (string-append #$output "/vendor/box2d/lib/"))
+                                                 (raylib-lib-out (string-append #$output "/vendor/raylib/linux/"))
+                                                 (glfw-lib-out (string-append #$output "/vendor/glfw/lib/"))
+                                                 (stb-libs-out (string-append #$output "/vendor/stb/lib"))
+                                                 (box2d-lib (string-append #$box2d+static "/lib/libbox2d.a"))
+                                                 (raylib-lib (string-append #$raylib-with-extras+static "/lib/libraylib.a"))
+                                                 (glfw-lib (string-append #$glfw+static "/lib/libglfw3.a"))
+                                                 (stb-libs (string-append (getcwd) "/vendor/stb/lib")))
+                                            (cond
+                                             ((string-prefix? "x86_64-linux" target-system)
+                                              (copy-file box2d-lib (string-append box2d-lib-out "box2d_other_amd64_avx2.a"))
+                                              (for-each (lambda (file)
+                                                          (install-file file stb-libs-out))
+                                                        (find-files stb-libs "\\.a$"))
+                                              (install-file raylib-lib raylib-lib-out)
+                                              (install-file glfw-lib glfw-lib-out))
+                                             (else
+                                              '())))
+                                          #t))
+                             (add-after 'install 'wrap-odin
+                                        (lambda* (#:key inputs outputs #:allow-other-keys)
+                                          (let* ((bin (string-append #$output "/bin/odin"))
+                                                 (llvm-lib (string-append #$llvm "/lib"))
+                                                 (clang-lib (string-append #$clang-toolchain "/lib")))
+                                            (wrap-program bin
+                                                          `("ODIN_ROOT" = (,#$output))
+                                                          `("LD_LIBRARY_PATH" = (,llvm-lib
+                                                                                 ,clang-lib))))
+                                          #t)))))
+     (native-inputs
+      (list llvm-18
+            clang-toolchain-18
+            ;;python-3
+            which
+            patchelf
+            box2d+static
+            glfw+static
+            raylib-with-extras+static))
+     (inputs
+      (list clang-toolchain-18))
+     (home-page "https://github.com/nakst/gf")
+     (synopsis "A modern, fast, and simple systems programming language")
+     (description
+      "The Odin programming language is a modern systems language focused on simplicity,
 performance, and productivity.  Designed as an alternative to C, it is suited for
 high-performance development, including game engines, graphics programming, and systems
 code.  Odin provides expressive syntax, support for data-oriented programming, a minimal
 runtime, strong compile-time efficiency, and seamless C interoperability.  This package
 includes the Odin compiler and standard library for building and running Odin programs.")
-      (license license:expat))))
+     (license license:expat))))
 
 ;; Uncomment to install with `guix package -f odin'
 ;; odin
