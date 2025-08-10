d271588fa31576871cc3aa6ed0b7c0f11b85d1e0
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2025-08-10 10:34:25 +0200

    Removed box2d package definition

--- bric-a-brac/packages/game-development.scm
+++ bric-a-brac/packages/game-development.scm
@@ -39,32 +39,31 @@
   #:use-module (gnu packages game-development)
   #:use-module (gnu packages pulseaudio)
   #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages gl)
-  #:export (box2d-3))
+  #:use-module (gnu packages gl))
 
-(define box2d-3
-  (package
-   (inherit box2d)
-   (name "box2d")
-   (version "3.0.0")
-   (source
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url "https://github.com/erincatto/box2d")
-           (commit (string-append "v" version))))
-     (file-name (git-file-name name version))
-     (sha256
-      (base32 "0m01c23mxvg96zypqyi2fpkd1dsvgflafi3ncga6ihdvxbwaybk5"))))
-   (build-system cmake-build-system)
-   (arguments
-    (substitute-keyword-arguments
-        (package-arguments box2d)
-      ((#:test-target _) "")
-      ((#:configure-flags original-flags)
-       `(cons* "-DBOX2D_UNIT_TESTS=OFF"
-               "-DBOX2D_SAMPLES=OFF"
-               (delete "-DBOX2D_BUILD_TESTBED=OFF" ,original-flags)))))))
+;; (define box2d-3
+;;   (package
+;;    (inherit box2d)
+;;    (name "box2d")
+;;    (version "3.0.0")
+;;    (source
+;;     (origin
+;;      (method git-fetch)
+;;      (uri (git-reference
+;;            (url "https://github.com/erincatto/box2d")
+;;            (commit (string-append "v" version))))
+;;      (file-name (git-file-name name version))
+;;      (sha256
+;;       (base32 "0m01c23mxvg96zypqyi2fpkd1dsvgflafi3ncga6ihdvxbwaybk5"))))
+;;    (build-system cmake-build-system)
+;;    (arguments
+;;     (substitute-keyword-arguments
+;;         (package-arguments box2d)
+;;       ((#:test-target _) "")
+;;       ((#:configure-flags original-flags)
+;;        `(cons* "-DBOX2D_UNIT_TESTS=OFF"
+;;                "-DBOX2D_SAMPLES=OFF"
+;;                (delete "-DBOX2D_BUILD_TESTBED=OFF" ,original-flags)))))))
 
 (define raylib-5.5
   (let ((commit "4f091f44a8d91d51019aa65c12da570435de450b")
