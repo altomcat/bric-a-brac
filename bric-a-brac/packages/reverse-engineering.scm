039b846162874af53dffb5d25a42be40d9248edb
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2026-05-08 09:11:50 +0200

    Fixed libemu release number to 1.0.5

--- bric-a-brac/packages/reverse-engineering.scm
+++ bric-a-brac/packages/reverse-engineering.scm
@@ -10,32 +10,33 @@
   #:export (libemu))
 
 (define libemu
-  (package
-    (name "libemu")
-    (version "0.2.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/buffer/libemu.git")
-             (commit "master")))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "1pjmr7285hdib3zqh6nbs1lzgb08byn5ayrgd6zvfw9bwsnf48fm"))))
-    (build-system gnu-build-system)
-    (native-inputs
-     (list autoconf automake libtool pkg-config))
-    (arguments
-     (list #:tests? #t
-           #:phases
-           #~(modify-phases %standard-phases
-               (add-after 'build 'remove-install-data
-                 (lambda _
-                   (substitute* "Makefile"
-                     (("install-data-am: install-pkgconfigDATA") ""))))
-               )))
-    (home-page "https://github.com/buffer/libemu")
-    (synopsis "Library for x86 shellcode emulation")
-    (description
-     "libemu is a library that provides x86 instruction emulation and shellcode analysis functionality.")
-    (license lgpl2.1+)))
+  (let ((commit "bf443cb08546db92f572b45738a5bb20259c7cad"))
+    (package
+      (name "libemu")
+      (version "1.0.5")
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/buffer/libemu.git")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1hn0yjsc66wwdmbzg9yi4pf1zxycywmc3gmpwz2qjycla7hzb8sn"))))
+      (build-system gnu-build-system)
+      (native-inputs
+       (list autoconf automake libtool pkg-config))
+      (arguments
+       (list #:tests? #t
+             #:phases
+             #~(modify-phases %standard-phases
+                 (add-after 'build 'remove-install-data
+                   (lambda _
+                     (substitute* "Makefile"
+                       (("install-data-am: install-pkgconfigDATA") ""))))
+                 )))
+      (home-page "https://github.com/buffer/libemu")
+      (synopsis "Library for x86 shellcode emulation")
+      (description
+       "libemu is a library that provides x86 instruction emulation and shellcode analysis functionality.")
+      (license lgpl2.1+))))
